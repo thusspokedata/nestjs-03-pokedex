139cdbc32ba00b233c871b6c767bac6bc14b49f1
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-09-07 12:32:48 +0200

    end section 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,11 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+  @MinLength(1)
+  @IsString()
+  name: string;
+}
